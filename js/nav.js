122b5243f6fb404604b2732beb142bc03bb03874
gonzaxav <68866316+gonzaxav@users.noreply.github.com>
2021-03-08 02:03:04 -0300

    update

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,26 @@
 document.addEventListener("DOMContentLoaded", function (e) {
     document.getElementById("navID").innerHTML =
     `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-darker">
         <div class="container">
-            <a class="navbar-brand d-none" href="start.html">Commision Xiran!</a>
+            <a class="navbar-brand" href="index.html">
+                &#9875; Commission Form
+            </a>
+                
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+    
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav w-100">
+                    <li class="nav-item col active d-flex align-items-center justify-content-center">
+                        <a class="nav-link" href="tos.html">
+                            Terms of Service
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
-  </nav>
+    </nav>
     `
 });
